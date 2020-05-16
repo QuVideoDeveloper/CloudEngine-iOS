24f97ea4e43546268ccb8e5e618bb95de5d66b01
Sunshine <cheng.xia@quvideo.com>
2020-05-16 20:59:15 +0800

    支持可配置 countryCode 和 languageCode

--- QVCloudEngine.podspec
+++ QVCloudEngine.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'QVCloudEngine'
-  s.version          = '1.1.12'
+  s.version          = '1.1.13'
   s.summary          = 'A short description of QVCloudEngine.'
 
 # This description is used to generate tags and improve search results.
--- QVCloudEngine/FrameWork/QVCloudEngineSDK.framework/Versions/A/Headers/QVCloudEngineConfig.h
+++ QVCloudEngine/FrameWork/QVCloudEngineSDK.framework/Versions/A/Headers/QVCloudEngineConfig.h
@@ -20,6 +20,13 @@ NS_ASSUME_NONNULL_BEGIN
 /// 必须参数 用户唯一标识或设备标识
 @property (nonatomic, copy) NSString *userId;
 
+/// 可选参数 默认获取系统的国家码
+@property (nonatomic, copy) NSString *countryCode;
+
+/// 可选参数 语言编码
+@property (nonatomic, copy) NSString *languageCode;
+
+
 @end
 
 NS_ASSUME_NONNULL_END
